12fedff1538444f42c0413cf888c1877a413547b
c00l9 <d3292550@gmail.com>
2025-01-08 14:34:49 +0500

    Create Fer.lua
    Jbjbh

---
+++ Fer.lua
@@ -0,0 +1,54 @@
+print("Under Scripting")
+setclipboard("https://[Log in to view URL] our website")
+
+--UI
+
+local Library = loadstring(game:HttpGet("https://[Log in to view URL]"))()
+local Window = Library.CreateLib("c00lkidd Reborn GUI v1  ", "BloodTheme")
+
+
+
+
+--GUI
+
+local fe = Window:NewTab("FE Bypass")
+local feSection = fe:NewSection("FE Bypass")
+
+feSection:NewButton("Spam Decals", "This Will Spam Decals", function()
+     local button = script.Parent
+local decals = {
+  "http://[Log in to view URL]",
+  "http://[Log in to view URL]",
+  "http://[Log in to view URL]",
+} 
+
+button.MouseButton1Down:Connect(function()
+  for _, decal in ipairs(decals) do
+    workspace.Decals:AddItem(Decal.new(decal))
+  end
+end)
+      
+end)
+
+
+
+
+
+
+-- Function
+
+
+
+  -- Spam Decals
+-- local button = script.Parent
+local decals = {
+  "http://[Log in to view URL]",
+  "http://[Log in to view URL]",
+  "http://[Log in to view URL]",
+} 
+
+button.MouseButton1Down:Connect(function()
+  for _, decal in ipairs(decals) do
+    workspace.Decals:AddItem(Decal.new(decal))
+  end
+end)
